1919cc65a11db8449abf32c41b203fee4773499e
ksenia <kseniya.uzlova@simbirsoft.com>
2018-11-25 16:45:19 +0300

    init

---
+++ src/components/App/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Auth from '../Auth';
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Auth/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Auth/auth.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './auth.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="auth">
+        auth
+      </div>
+    );
+  }
+}
+
+export default App;
