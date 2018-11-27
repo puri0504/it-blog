e941e82d3ad69fadfb5ab5aafa05481c0f7b2dc1
ksenia <kseniya.uzlova@simbirsoft.com>
2018-11-27 23:13:07 +0300

    build

---
+++ build/precache-manifest.086b60d777b5dbfaacf777ede90d6817.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c382f0b01ed4b1d319100d2f6789005",
+    "url": "/static/media/google_icon.9c382f0b.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4ce825528e4925182eea",
+    "url": "/static/js/main.4ce82552.chunk.js"
+  },
+  {
+    "revision": "8f7140a83ca32778ca8c",
+    "url": "/static/js/1.8f7140a8.chunk.js"
+  },
+  {
+    "revision": "4ce825528e4925182eea",
+    "url": "/static/css/main.1ec83ebc.chunk.css"
+  },
+  {
+    "revision": "8f7140a83ca32778ca8c",
+    "url": "/static/css/1.0cbec10c.chunk.css"
+  },
+  {
+    "revision": "c32a8cc36a095db791bd70e775070ba2",
+    "url": "/index.html"
+  }
+];
